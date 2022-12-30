bd4844765f81c4bc920564135d323bade457b3bc
Kevin Cai <kevincai@kevins-air.home>
2022-12-30 14:20:06 +0000

    added timeline

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import './App.css';
-import kev from './imgs/kev-5.jpeg'; // import your image file
+import kev from './imgs/kev-5.jpeg';
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 
 const projectStyle = {
   display: 'grid',
   gridTemplateColumns: '1fr 1fr', // Create two columns
   gridTemplateRows: '1fr', // Create one row
   gridGap: '20px', // Add a gap between the grid items
+};
+
+const Contact = () => {
+  const contactStyle = {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr 1fr', // Create three columns
+    gridTemplateRows: '1fr 1fr', // Create two rows
+    gridGap: '20px', // Add a gap between the grid items
+  };
+
+  return (
+    <div className="contact" style={contactStyle}>
+      <a href="[your GitHub profile URL]"><FaGithub style={{ fontSize: '48px' }} /></a>
+      <a href="[your LinkedIn profile URL]"><FaLinkedin style={{ fontSize: '48px' }} /></a>
+      <a href="[your email address]"><FaEnvelope style={{ fontSize: '48px' }} /></a>
+      <p>GitHub</p>
+      <p>LinkedIn</p>
+      <p>Email</p>
+    </div>
+  );
 };
 
 function App() {
@@ -38,6 +59,42 @@ function App() {
           </div>
           <div/>
         </section>
+        <section id="timeline">
+          <div className="timeline-container">
+            <div className="timeline">
+              <div className="timeline-event">
+                <h3>Year 1</h3>
+                <p>
+                  [Brief description of event 1]
+                </p>
+              </div>
+              <div className="timeline-event">
+                <h3>Year 2</h3>
+                <p>
+                  [Brief description of event 2]
+                </p>
+              </div>
+              <div className="timeline-event">
+                <h3>Year 3</h3>
+                <p>
+                  [Brief description of event 3]
+                </p>
+              </div>
+              <div className="timeline-event">
+                <h3>Year 4</h3>
+                <p>
+                  [Brief description of event 4]
+                </p>
+              </div>
+              <div className="timeline-event">
+                <h3>Year 5</h3>
+                <p>
+                  [Brief description of event 5]
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
         <section id="education">
           <div className="section-content">
             <h2>Education</h2>
@@ -62,15 +119,10 @@ function App() {
           </div>
         </section>
         <section id="contact">
-          <h2>Contact</h2>
-          <p>
-            You can reach me at [your email address] or through my social media links:
-            <ul>
-              <li><a href="[your LinkedIn profile URL]">LinkedIn</a></li>
-              <li><a href="[your Twitter handle]">Twitter</a></li>
-              <li><a href="[your GitHub profile URL]">GitHub</a></li>
-            </ul>
-          </p>
+          <div className="section-content">
+            <h2>Contact</h2>
+            <Contact />
+          </div>
         </section>
       </main>
     </div>
